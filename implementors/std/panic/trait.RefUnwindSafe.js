687b7b5f2bf81f6f2c2953c941c9663286c9dc6d
zhuhaow <zhuhaow@users.noreply.github.com>
2022-04-25 15:25:40 +0000

    deploy: c01ab5f20ef681882b184d04dc61f364911ccf93

--- implementors/std/panic/trait.RefUnwindSafe.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["dynomite"] = [{"text":"impl RefUnwindSafe for AttributeError","synthetic":true,"types":[]},{"text":"impl&lt;D&gt; RefUnwindSafe for RetryingDynamoDb&lt;D&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;D: RefUnwindSafe,&nbsp;</span>","synthetic":true,"types":[]},{"text":"impl RefUnwindSafe for Policy","synthetic":true,"types":[]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- implementors/std/panic/trait.UnwindSafe.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["dynomite"] = [{"text":"impl UnwindSafe for AttributeError","synthetic":true,"types":[]},{"text":"impl&lt;D&gt; UnwindSafe for RetryingDynamoDb&lt;D&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;D: RefUnwindSafe,&nbsp;</span>","synthetic":true,"types":[]},{"text":"impl UnwindSafe for Policy","synthetic":true,"types":[]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
